1662357d78323545bfcf421335d814069d4763bb
David Baruteau <Predalien2000@hotmil.com>
2018-03-20 11:08:07 +0100

    update servo comand (add interpolation)

--- backend/servo_test.py
+++ backend/servo_test.py
@@ -3,20 +3,24 @@ import modules.servo as servo
 import time
 import RPi.GPIO as GPIO
 import atexit
-atexit.register(GPIO.cleanup)  
+  
+
+myServo = servo.Servo(pd.ServoPin)
+
+def  cleanup():
+	myServo.cleanup()
+	GPIO.cleanup()
+
+atexit.register(cleanup)
+
 
 def main():
-	print pd.ServoPin	
-	myServo = servo.Servo(pd.ServoPin)
-	angles = [10, 180, 45, 135, 90]
-	for i in range(5):
-		for angle in angles:
-			print "Setting angle", angle
-			myServo.setAngle(angle)
-	myServo.cleanup();
+	angles = [0, 45, 90, 135, 180, 135, 90, 45]
+	#while(True):
+	for angle in angles:
+		myServo.setAngle(angle)
+		time.sleep(3)
 
-	
-	
 	
 if __name__ == "__main__":
 	main()
