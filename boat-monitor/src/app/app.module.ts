27fabc3ff568c3249d6a22e70558c6aa2111aaf7
Sebastien Sable <SSable@slb.com>
2018-03-21 15:14:25 +0100

    Load data from json file and show markers on map

--- boat-monitor/src/app/app.module.ts
+++ boat-monitor/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
-
-
+import { PositionService } from './services/position.service';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
 import { BoatMapComponent } from './boat-map/boat-map.component';
@@ -14,11 +15,12 @@ import { BoatMapComponent } from './boat-map/boat-map.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCOh39h8mm8x3tsXOidh6weLnbZEC6gebU'
     })
   ],
-  providers: [],
+  providers: [PositionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- boat-monitor/src/app/boat-map/boat-map.component.ts
+++ boat-monitor/src/app/boat-map/boat-map.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { PositionService } from '../services/position.service';
+import { HttpClient } from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
-import { AgmKmlLayer } from '@agm/core/directives';
+import { Position } from '../model/position';
+
 
 @Component({
   selector: 'app-boat-map',
+  providers: [ PositionService, HttpClient ],
   templateUrl: './boat-map.component.html',
   styleUrls: ['./boat-map.component.css'],
 })
@@ -11,6 +15,7 @@ export class BoatMapComponent implements OnInit {
   title: string = 'Boat locator';
   lat: number = 43.616040;
   lng: number = 3.907910;
+  positions: any[];
   current_marker_lat: number = null;
   current_marker_lng: number = null;
   dest_marker_lat: number = null;
@@ -18,11 +23,16 @@ export class BoatMapComponent implements OnInit {
 
   destChosen = false;
 
-  constructor() { }
+  constructor(private positionService: PositionService) { }
 
   ngOnInit() {
+    this.positions = [];
     this.current_marker_lat = 43.616040;
     this.current_marker_lng = 3.907910;
+
+    this.positionService.getPositions().subscribe((positions: Position[]) => {
+      this.updatePositions(positions);
+    });
   }
 
   onChoseLocation(event) {
@@ -32,4 +42,7 @@ export class BoatMapComponent implements OnInit {
     this.destChosen = true;
   }
 
+  updatePositions(positions) {
+    this.positions = positions;
+  }
 }
