4e5095344a05f84f963235554dfd9d3864bd90db
AlessandroBottoni <abottoni@slb.com>
2018-03-22 14:47:51 +0000

    config

--- navigation/main.py
+++ navigation/main.py
@@ -1,6 +1,5 @@
 import sys
-sys.path.insert(0, 'D:/Python/PycharmProjects/iSea2018Nest01/backend')
-#sys.path.insert(0, '/home/pi/gitRepo/ISea2018Nest01/backend')
+sys.path.insert(0, '/home/pi/gitRepo/ISea2018Nest01/backend')
 import pindefinition as pd
 import route as Routing
 import trace as Gps
@@ -13,27 +12,29 @@ import os
 import modules.motorpi as Motor
 
 def main():
-    print('hello isea')
-    fileDir = os.path.dirname(os.path.realpath(__file__))
+    try:
+        print('hello isea')
+        fileDir = os.path.dirname(os.path.realpath(__file__))
 
-    motor = Motor.MotorPI(pd.MotorPin)
-    motor.setNeutral()
+        motor = Motor.MotorPI(pd.MotorPin)
+        motor.setNeutral()
 
-    route = Routing.Route()
-    route.initFromFile(os.path.join(fileDir,'files/route.json'))
+        route = Routing.Route()
+        route.initFromFile(os.path.join(fileDir,'files/route.json'))
 
-    #boat = Boat()
-    #boat.start()
-    servopi = ServoPI(pd.ServoPin)
-    Gps.TraceThread(os.path.join(fileDir,'files/trace.json')).start()
+        #boat = Boat()
+        #boat.start()
+        servopi = ServoPI(pd.ServoPin)
+        Gps.TraceThread(os.path.join(fileDir,'files/trace.json')).start()
 
-    controller = Controller.Steering(route, servopi, motor)
-    controller.start()
-    controller.join()
+        controller = Controller.Steering(route, servopi, motor)
+        controller.start()
+        controller.join()
 
-    Kml.exportToKML(route, os.path.join(fileDir,'files/trace.json'), os.path.join(fileDir,'files/route.kml'), os.path.join(fileDir,'files/trace.kml'))
+        Kml.exportToKML(route, os.path.join(fileDir,'files/trace.json'), os.path.join(fileDir,'files/route.kml'), os.path.join(fileDir,'files/trace.kml'))
 
-
+    finally:
+        motor.cleanUp()
 
 if __name__ == '__main__':
     main()
